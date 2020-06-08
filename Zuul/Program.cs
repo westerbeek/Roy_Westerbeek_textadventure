64c4fdad3ecdcafa98900c1eacc2a7a52c83649b
westerbeek <roy-westerbeek@hotmail.com>
2020-06-08 10:57:32 +0200

    1stbuild
    hallo!

---
+++ Zuul/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Zuul
+{
+    class Program
+    {
+        /**
+		 * Create and play the Game.
+		 */
+        public static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.play();
+
+            // close the window with 'enter'
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Zuul/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Zuul")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Zuul")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d5a4446c-ff0c-4c6d-b800-e58d08e7d5b9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
